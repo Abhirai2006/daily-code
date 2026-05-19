93bbe5d1883d51338185edec1fadc969425b5683
Abhishek Rai A <abhirai2006@gmail.com>
2026-05-19 21:46:09 +0530

    Create 02_Classes_Objects_&_self.py

---
+++ Python/Object_Oriented_Programming/02_Classes_Objects_&_self.py
@@ -0,0 +1,28 @@
+# =========================================
+# Python OOPs: Classes, Objects & self
+# =========================================
+
+# In this file, we will learn:
+# - Classes
+# - Objects
+# - Methods
+# - self keyword
+# - How objects work internally
+
+
+# =========================================
+# Understanding Classes and Objects
+# =========================================
+
+# A class is a blueprint.
+
+# An object is an instance of a class.
+
+# Example:
+# Class  -> Car
+# Object -> BMW, Audi, Tesla
+
+
+# =========================================
+# Creating a Simple Class
+# =========================================
