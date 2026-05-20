afa57502804b804a6ea10daa243dfe3d8b81c5c1
Abhirai2006 <abhirai2006@gmail.com>
2026-05-20 20:17:25 +0530

    create day 03 file

---
+++ Python/Object_Oriented_Programming/03_Constructors_&___init__().py
@@ -0,0 +1,28 @@
+# =========================================
+# Python OOPs: Constructors & __init__()
+# =========================================
+
+# In this file, we will learn:
+# - Constructors
+# - __init__() method
+# - Object initialization
+# - Passing values to objects
+# - Constructor overriding
+
+
+# =========================================
+# What is a Constructor?
+# =========================================
+
+# A constructor is a special method
+# that runs automatically when an object
+# is created.
+
+# In Python, the constructor method is:
+# __init__()
+
+# Purpose of constructor:
+# - Initialize object data
+# - Assign values to objects
+# - Reduce repetitive code
+
