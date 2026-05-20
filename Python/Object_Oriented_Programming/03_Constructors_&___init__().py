2dbfc853682665fd8868801b44dc076e762fcc4e
Abhirai2006 <abhirai2006@gmail.com>
2026-05-20 20:27:33 +0530

    Initializing Object Data

--- Python/Object_Oriented_Programming/03_Constructors_&___init__().py
+++ Python/Object_Oriented_Programming/03_Constructors_&___init__().py
@@ -44,3 +44,29 @@ s1 = Student()
 # Constructor runs automatically
 # when object is created.
 
+
+# =========================================
+# Initializing Object Data
+# =========================================
+
+class Employee:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# ----- Creating Employee Objects -----
+
+print("\n----- Creating Employee Objects -----")
+
+e1 = Employee("Rahul", 23)
+e2 = Employee("Anjali", 25)
+
+print("Employee 1 Name :", e1.name)
+print("Employee 1 Age  :", e1.age)
+
+print()
+
+print("Employee 2 Name :", e2.name)
+print("Employee 2 Age  :", e2.age)
+
