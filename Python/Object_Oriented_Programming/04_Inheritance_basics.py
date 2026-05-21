39864e4d464f9fb32b04ac54c4107ea57fbc4335
Abhirai2006 <abhirai2006@gmail.com>
2026-05-21 21:02:12 +0530

    Create 04_Inheritance_basics.py
    Inheritance basics

---
+++ Python/Object_Oriented_Programming/04_Inheritance_basics.py
@@ -0,0 +1,29 @@
+# =========================================
+# Python OOPs: Inheritance Basics
+# =========================================
+
+# In this file, we will learn:
+# - Inheritance
+# - Parent Class
+# - Child Class
+# - Code Reusability
+# - Reusing Methods and Variables
+
+
+# =========================================
+# What is Inheritance?
+# =========================================
+
+# Inheritance allows one class
+# to acquire the properties and
+# behaviors of another class.
+
+# In simple words:
+# A child class can reuse code
+# from a parent class.
+
+# Benefits:
+# - Code Reusability
+# - Cleaner Programs
+# - Easier Maintenance
+# - Better Organization
