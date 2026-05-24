4f6dba67a2771cc9467d12d8e6c0546d7bedd6ca
Abhirai2006 <abhirai2006@gmail.com>
2026-05-24 16:58:59 +0530

    Create 06_Encapsulation.py
    Encapsulation

---
+++ Python/Object_Oriented_Programming/06_Encapsulation.py
@@ -0,0 +1,30 @@
+# =========================================
+# Python OOPs: Encapsulation
+# =========================================
+
+# In this file, we will learn:
+# - Encapsulation
+# - Public Variables
+# - Protected Variables
+# - Private Variables
+# - Name Mangling
+# - Getters and Setters
+# - Data Hiding
+
+
+# =========================================
+# What is Encapsulation?
+# =========================================
+
+# Encapsulation means wrapping
+# data (variables) and methods
+# (functions) together inside a class.
+
+# It also helps restrict direct
+# access to important data.
+
+# Main Goals:
+# - Protect Data
+# - Control Access
+# - Hide Internal Details
+
