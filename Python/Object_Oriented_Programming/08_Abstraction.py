7d509976b3e07652e42304123f521fd36f62d444
Abhirai2006 <abhirai2006@gmail.com>
2026-05-26 05:08:54 +0530

    Create 08_Abstraction.py
    Abstraction

---
+++ Python/Object_Oriented_Programming/08_Abstraction.py
@@ -0,0 +1,44 @@
+# =========================================
+# Python OOPs: Abstraction
+# =========================================
+
+# In this file, we will learn:
+# - Abstraction
+# - Abstract Classes
+# - Abstract Methods
+# - ABC Module
+# - Hiding Internal Implementation
+
+
+# =========================================
+# What is Abstraction?
+# =========================================
+
+# Abstraction means:
+# Hiding internal implementation details
+# and showing only the essential features
+# to the user.
+
+# The user only knows:
+# - What to do
+
+# The user does not need to know:
+# - How it works internally
+
+
+# =========================================
+# Real World Example
+# =========================================
+
+# ATM Machine:
+# We only:
+# - Insert card
+# - Enter PIN
+# - Withdraw money
+
+# We do not know:
+# - Server communication
+# - Database operations
+# - Security checks
+
+# This is abstraction.
