219df7c258743cebb207c01da3c668f8145f997f
Abhirai2006 <abhirai2006@gmail.com>
2026-05-25 07:36:12 +0530

    Create 07_Polymorphism.py
    Polymorphism

---
+++ Python/Object_Oriented_Programming/07_Polymorphism.py
@@ -0,0 +1,26 @@
+# =========================================
+# Python OOPs: Polymorphism
+# =========================================
+
+# In this file, we will learn:
+# - Polymorphism
+# - Function Polymorphism
+# - Method Overriding
+# - Runtime Polymorphism
+# - Same Method, Different Behavior
+
+
+# =========================================
+# What is Polymorphism?
+# =========================================
+
+# Polymorphism means:
+# One thing can have many forms.
+
+# Poly  -> Many
+# Morph -> Forms
+
+# In Python:
+# Same function or method name
+# can behave differently
+# depending on the object or data.
