7f39a11f99f431f81fee6959159f767398828c73
Abhirai2006 <abhirai2006@gmail.com>
2026-05-28 05:21:07 +0530

    Answer 05 - Library Management System

--- Python/Object_Oriented_Programming/Assignments_Answers.py
+++ Python/Object_Oriented_Programming/Assignments_Answers.py
@@ -152,3 +152,55 @@ employees = [
 for emp in employees:
     print(emp.name, "->", emp.category())
 
+
+# =========================================
+# Answer 05
+# Library Management System
+# =========================================
+
+class Book:
+    def __init__(self, title):
+        self.title = title
+        self.available = True
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+        print(f"{book.title} Added Successfully")
+
+
+    def issue_book(self, title):
+        for book in self.books:
+            if book.title == title and book.available:
+                book.available = False
+                print(f"{title} Issued Successfully")
+                return
+        print("Book Not Available")
+
+
+    def return_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                book.available = True
+                print(f"{title} Returned Successfully")
+                return
+
+
+# ----- Library Management Example -----
+
+print("\n----- Library Management Example -----")
+
+lib = Library()
+
+b1 = Book("Python")
+b2 = Book("Java")
+
+lib.add_book(b1)
+lib.add_book(b2)
+
+lib.issue_book("Java")
+lib.return_book("Java")
+
