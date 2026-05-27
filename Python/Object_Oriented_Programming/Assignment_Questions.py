a786f3fe23a503ef794bffd15309190507261a57
Abhirai2006 <abhirai2006@gmail.com>
2026-05-27 05:35:26 +0530

    Create Assignment_Questions.py

---
+++ Python/Object_Oriented_Programming/Assignment_Questions.py
@@ -0,0 +1,15 @@
+# =========================================
+# Python OOPs: Assignment Questions
+# =========================================
+
+# This file contains beginner-friendly
+# OOPs practice questions.
+
+# Topics Covered:
+# - Classes & Objects
+# - Constructors
+# - Inheritance
+# - Encapsulation
+# - Polymorphism
+# - Abstraction
+
