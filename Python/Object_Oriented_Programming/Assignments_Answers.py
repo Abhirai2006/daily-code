4f61d2a5be98bf1585a343e588f3468fc1faaca5
Abhirai2006 <abhirai2006@gmail.com>
2026-05-28 05:09:32 +0530

    Answer 01 - Abstract Class

--- Python/Object_Oriented_Programming/Assignments_Answers.py
+++ Python/Object_Oriented_Programming/Assignments_Answers.py
@@ -13,3 +13,39 @@
 # - Polymorphism
 # - Abstraction
 
+
+# =========================================
+# Answer 01
+# Abstract Class - Remote Control
+# =========================================
+
+from abc import ABC, abstractmethod
+
+
+class RemoteControl(ABC):
+    @abstractmethod
+    def power_on(self):
+        pass
+
+    @abstractmethod
+    def power_off(self):
+        pass
+
+
+class TVRemote(RemoteControl):
+    def power_on(self):
+        print("TV is ON")
+
+    def power_off(self):
+        print("TV is OFF")
+
+
+# ----- Remote Control Example -----
+
+print("\n----- Remote Control Example -----")
+
+remote = TVRemote()
+
+remote.power_on()
+remote.power_off()
+
